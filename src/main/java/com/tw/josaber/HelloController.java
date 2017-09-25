0c64b67e1acdf70a705fb68745005fe57bc9ebba
Josaber <bqliu@thoughtworks.com>
2017-09-25 14:03:24 +0800

    [Josaber] Extract SpringApplication.run() to Application.java file.

--- src/main/java/com/tw/josaber/HelloController.java
+++ src/main/java/com/tw/josaber/HelloController.java
@@ -1,6 +1,5 @@
 package com.tw.josaber;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +13,5 @@ public class HelloController {
     @ResponseBody
     String hello() {
         return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(HelloController.class, args);
     }
 }
