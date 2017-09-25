20812c2420c9d36ff4d973e0837a4389ed53ce20
Josaber <bqliu@thoughtworks.com>
2017-09-25 15:46:27 +0800

    [Josaber] Add ResponseMessage with code status and message.

---
+++ src/main/java/com/tw/josaber/model/ResponseMessage.java
@@ -0,0 +1,30 @@
+package com.tw.josaber.model;
+
+public class ResponseMessage {
+    private int code;
+    private String message;
+    private TodoItem todoItem;
+
+    public ResponseMessage(int code, String message, TodoItem todoItem) {
+        this.code = code;
+        this.message = message;
+        this.todoItem = todoItem;
+    }
+
+    public ResponseMessage(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public TodoItem getTodoItem() {
+        return todoItem;
+    }
+}
