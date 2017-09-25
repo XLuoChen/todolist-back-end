72bbde63506e9d01bdd76f5d6f9f4326de8aff8b
Josaber <bqliu@thoughtworks.com>
2017-09-25 16:34:22 +0800

    [Josaber] Add Synatic Sugar for Response Message.

---
+++ src/main/java/com/tw/josaber/utils/SynaxSugar.java
@@ -0,0 +1,15 @@
+package com.tw.josaber.utils;
+
+public class SynaxSugar {
+
+    public static final int CREATE_SUCC_CODE = 201;
+    public static final String CREATE_SUCC_MESSAGE = "Create Todo Item Successfully!";
+    public static final int UPDATE_SUCC_CODE = 200;
+    public static final String UPDATE_SUCC_MESSAGE = "Update Todo Item Successfully!";
+    public static final int UPDATE_FAIL_CODE = 404;
+    public static final String UPDATE_FAIL_MESSAGE = "The Todo Item Is Not Found!";
+    public static final int DELETE_SUCC_CODE = 204;
+    public static final String DELETE_SUCC_MESSAGE = "Delete Todo Item Successfully!";
+    public static final int DELETE_FAIL_CODE = 404;
+    public static final String DELETE_FAIL_MESSAGE = "Fail to Delete Todo Item!";
+}
