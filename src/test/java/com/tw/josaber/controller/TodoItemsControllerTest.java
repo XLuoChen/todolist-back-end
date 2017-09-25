77e2a058b42fc082cbdcb373769255c7bec8c490
Josaber <bqliu@thoughtworks.com>
2017-09-25 14:35:15 +0800

    [Josaber] Add todo items controller test: TodoItemsControllerTest and add dependencies: junit and mockito.

---
+++ src/test/java/com/tw/josaber/controller/TodoItemsControllerTest.java
@@ -0,0 +1,15 @@
+package com.tw.josaber.controller;
+
+import org.junit.Before;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+
+public class TodoItemsControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
