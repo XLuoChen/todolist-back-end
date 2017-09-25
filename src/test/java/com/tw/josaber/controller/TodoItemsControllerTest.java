489c6a058143298a55063a4ec79b4d1707b18582
Josaber <bqliu@thoughtworks.com>
2017-09-25 23:26:17 +0800

    [Josaber] Add todoItem to controller test.

--- src/test/java/com/tw/josaber/controller/TodoItemsControllerTest.java
+++ src/test/java/com/tw/josaber/controller/TodoItemsControllerTest.java
@@ -1,6 +1,8 @@
 package com.tw.josaber.controller;
 
+import com.tw.josaber.entity.TodoItem;
 import com.tw.josaber.service.TodoItemService;
+import com.tw.josaber.utils.DateTimeUtil;
 import org.junit.Before;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -10,13 +12,18 @@ public class TodoItemsControllerTest {
 
     @Mock
     TodoItemService todoItemService;
-
     TodoItemsController todoItemsController;
+    TodoItem todoItem;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         todoItemsController = new TodoItemsController(todoItemService);
+
+        todoItem = new TodoItem()
+                .setText("test todo item")
+                .setDone(false)
+                .setTimestamp(DateTimeUtil.getCurrentTime());
     }
 
 }
