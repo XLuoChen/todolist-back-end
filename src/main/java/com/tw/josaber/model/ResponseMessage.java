bb6695085665c3e972773fd1fd7b682852ef90af
Josaber <bqliu@thoughtworks.com>
2017-09-26 09:54:18 +0800

    [Josaber] Override the equals and hashcode methods.

--- src/main/java/com/tw/josaber/model/ResponseMessage.java
+++ src/main/java/com/tw/josaber/model/ResponseMessage.java
@@ -29,4 +29,22 @@ public class ResponseMessage {
     public TodoItem getTodoItem() {
         return todoItem;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ResponseMessage message1 = (ResponseMessage) o;
+
+        if (status != message1.status) return false;
+        return message != null ? message.equals(message1.message) : message1.message == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = status;
+        result = 31 * result + (message != null ? message.hashCode() : 0);
+        return result;
+    }
 }
