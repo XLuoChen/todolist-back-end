efaefad371a2707ee8603a9914a877c0a9123d96
Chengxiu Luo <cxluo@thoughtworks.com>
2017-11-01 09:56:16 +0800

    config develping env with docker

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+today=`date +%Y%m%d`
+
+mvn clean package
+
+docker build -t chengxiuluo/todolist-backend:${today} .
+docker build -t chengxiuluo/todolist-backend:latest .
+
+docker push chengxiuluo/todolist-backend:${today}
+docker push chengxiuluo/todolist-backend:latest
