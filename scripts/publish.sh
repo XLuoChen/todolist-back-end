10933b9bcf0606bbdde471f967a23147e7a62aae
Chengxiu Luo <cxluo@thoughtworks.com>
2017-11-02 08:52:27 +0800

    add super permission

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,11 +4,11 @@ today=`date +%Y%m%d`
 
 mvn clean package
 
-docker build -t chengxiuluo/todolist-backend:${today} .
-docker build -t chengxiuluo/todolist-backend:latest .
+sudo docker build -t chengxiuluo/todolist-backend:${today} .
+sudo docker build -t chengxiuluo/todolist-backend:latest .
 
-docker push chengxiuluo/todolist-backend:${today}
-docker push chengxiuluo/todolist-backend:latest
+sudo docker push chengxiuluo/todolist-backend:${today}
+sudo docker push chengxiuluo/todolist-backend:latest
 
 #docker rmi chengxiuluo/todolist-backend:${today}
 #docker rmi chengxiuluo/todolist-backend:latest
